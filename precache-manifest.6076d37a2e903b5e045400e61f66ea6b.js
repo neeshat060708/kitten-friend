85bb3a52d3ddaa1e93b79e16d3b2a98ae4406278
neeshat060708 <neeshat060708@gmail.com>
2023-09-22 23:40:37 -0400

    Updates

---
+++ precache-manifest.6076d37a2e903b5e045400e61f66ea6b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e8e2a102041a8662595",
+    "url": "/kitten-friend/static/css/main.7da1963b.chunk.css"
+  },
+  {
+    "revision": "4e8e2a102041a8662595",
+    "url": "/kitten-friend/static/js/main.4e8e2a10.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/kitten-friend/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "df6b12fae98a76b56953",
+    "url": "/kitten-friend/static/css/2.08e3541a.chunk.css"
+  },
+  {
+    "revision": "df6b12fae98a76b56953",
+    "url": "/kitten-friend/static/js/2.df6b12fa.chunk.js"
+  },
+  {
+    "revision": "25af682b550207fade97",
+    "url": "/kitten-friend/static/js/runtime~main.25af682b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kitten-friend/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fbcbffe08604eb913b5f4dd95a508cde",
+    "url": "/kitten-friend/index.html"
+  }
+];
